a90a95e14833e73c7066675c9170396d1bbac9f3
Guilherme Bertuol <guilherme.bertuol@gmail.com>
2012-09-23 23:31:33 -0300

    Added code to interactive menu

--- Main.cpp
+++ Main.cpp
@@ -9,9 +9,17 @@
 #include <sstream>
 #include <iostream>
 #include <fstream>
-#include <stdio.h>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
 #include "Stack.h"
 
+static const char CREATE_STR[] = "Criar";
+static const char INSERT_STR[] = "Inserir";
+static const char REMOVE_STR[] = "Remover";
+static const char SHOW_STR[] = "Mostrar";
+static const char EXIT_STR[] = "Sair";
+static const char HELP_STR[] = "Ajuda";
 
 static void commandInsert(std::istream &stream, std::istream &file, Stack &stack)
 {
@@ -39,101 +47,178 @@ static void commandShow(Stack &stack)
 
 }
 
-//void menu() {
-//
-//	// WHAT TO DO HERE
-//	bool isOn = true;
-//	while (isOn) {
-//
-//
-//		cin >> input >> input2;
-//
-//		//Initializes the stack.
-//		if (input == "CRIAR") {
-//			cout << stack.currentTop() << endl;
-//
-//			stack.initializeStack();
-//
-//		} // Inserts certain amount of data after it's informed as the parameter 'input2'. The command is INSERIR <size>.
-//		else if (input == "INSERIR") {
-//
-//			stringstream stream;
-//			int number;
-//			stream << input2;
-//			stream >> number;
-//
-//			for (int i = 0; i < number; i++) {
-//				stack.push(i);
-//			}
-//
-//		} //Removes data from the top of the Stack.Only.
-//		else if (input == "REMOVER") {
-//
-//			stack.remove();
-//
-//		} //Shows all data within the Stack, according to its properties. (Top element is the first and bottom is the last one).
-//		else if (input == "MOSTRAR") {
-//			for (int i = stack.currentTop(); i >= 0; i--) {
-//				cout << stack.getElement(i) << endl;
-//			}
-//
-//		} //Exits the program.
-//		else if (input == "SAIR") {
-//			isOn = false;
-//			break;
-//		}
-//
-//	}
-//
-//}
+static void printPrompt()
+{
+	std::cout << "Comando: ";
+}
+
+static void printInitialHelp()
+{
+	std::cout << "Supported commands: " << std::endl
+			<< "\t\""<< CREATE_STR << "\"" << std::endl
+			<< "\t\""<< HELP_STR << "\"" << std::endl
+			<< "\t\""<< EXIT_STR << "\"" << std::endl;
+}
+
+static void printHelp()
+{
+	std::cout << "Supported commands: " << std::endl
+			<< "\t\""<< INSERT_STR << "\" N (N > 0)" << std::endl
+			<< "\t\""<< REMOVE_STR << "\"" << std::endl
+			<< "\t\""<< SHOW_STR << "\"" << std::endl
+			<< "\t\""<< HELP_STR << "\"" << std::endl
+			<< "\t\""<< EXIT_STR << "\"" << std::endl;
+}
+
+static void printError(const std::string &command)
+{
+	std::cout << "!!! Comando inválido: \"" << command << "\"" << std::endl;
+}
+
+static void printEmptyStack()
+{
+	std::cout << "*** Pilha vazia!" << std::endl;
+}
+
+// return true if error
+bool toInt(int &val, const std::string &string)
+{
+	const char *start = string.c_str();
+	char *end;
+	val = strtol(start, &end, 10);
+	return end == start|| *end != '\0';
+}
+
+static void interactiveExecute(Stack &stack)
+{
+	bool isCreated = false;
+	while (true)
+	{
+		if (!isCreated)
+			printInitialHelp();
+		else
+			printHelp();
+
+		printPrompt();
+
+		std::string command;
+		std::getline(std::cin, command);
+
+		if (strcasecmp(command.c_str(), EXIT_STR) == 0)
+		{
+			std::cout << "Bye" << std::endl;
+			break;
+		}
+		else if (strcasecmp(command.c_str(), HELP_STR) == 0)
+		{
+			continue;
+		}
+		else
+		{
+			std::stringstream stream(command);
+
+			std::string subCommand;
+			stream >> subCommand;
+
+			if (isCreated)
+			{
+				if (strcasecmp(subCommand.c_str(), INSERT_STR) == 0)
+				{
+					int n;
+					stream >> n;
+
+					for (int i = 0; i < n; ++i)
+					{
+						int val;
+						do
+						{
+							std::cout << "[" << i << "/" << n << "]: ";
+							std::getline(std::cin, subCommand);
+						}
+						while (toInt(val, subCommand));
+
+						stack.push(val);
+					}
+				}
+				else if (strcasecmp(subCommand.c_str(), REMOVE_STR) == 0)
+				{
+					stack.remove();
+					if (stack.emptyStack())
+					{
+						isCreated = false;
+						printEmptyStack();
+					}
+				}
+				else if (strcasecmp(subCommand.c_str(), SHOW_STR) == 0)
+				{
+					commandShow(stack);
+				}
+				else
+				{
+					printError(command);
+				}
+			}
+			else if (strcasecmp(subCommand.c_str(), CREATE_STR) == 0)
+			{
+				stack.initializeStack();
+				isCreated = true;
+			}
+			else
+			{
+				printError(command);
+			}
+		}
+	}
+}
+
+static void executeFromFile(Stack &stack, const std::string &filename)
+{
+	std::ifstream file(filename.c_str());
+	std::string line;
+
+	//While file is not finished
+	while (std::getline(file, line))
+	{
+		std::stringstream stream(line);
+		std::string command = line;
+		stream >> command;
+
+		if (command == "CRIAR")
+		{
+			stack.initializeStack();
+		}
+		else if (command == "INSERIR")
+		{
+			commandInsert(stream, file, stack);
+		}
+		else if (command == "REMOVER")
+		{
+			stack.remove();
+		}
+		else if (command == "MOSTRAR")
+		{
+			commandShow(stack);
+
+		}
+		else if (command == "SAIR")
+		{
+			file.close();
+			break;
+		}
+	}
+}
 
 int main(int argc, char** argv)
 {
 	Stack stack;
 
-	// If number of parameters is lesser to 2, it runs menu(), else it reads the TESTCASE subroutine.
 	if (argc < 2)
 	{
-//		menu();
-		return -1;
+		interactiveExecute(stack);
 	}
 	else
 	{
-		std::ifstream file(argv[1]);
-		std::string line;
-
-		//While file is not finished
-		while (std::getline(file, line))
-		{
-			std::stringstream stream(line);
-			std::string command = line;
-			stream >> command;
-
-			if (command == "CRIAR")
-			{
-				stack.initializeStack();
-			}
-			else if (command == "INSERIR")
-			{
-				commandInsert(stream, file, stack);
-			}
-			else if (command == "REMOVER")
-			{
-				stack.remove();
-
-			}
-			else if (command == "MOSTRAR")
-			{
-				commandShow(stack);
-
-			}
-			else if (command == "SAIR")
-			{
-				file.close();
-				break;
-			}
-
-		}
+		executeFromFile(stack, argv[1]);
 	}
 }
 
